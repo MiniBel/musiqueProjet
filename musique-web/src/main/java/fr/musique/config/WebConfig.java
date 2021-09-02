f6ec3b4b9eb5f19b49873eb3eb45a9363346b60c
benoit <benoit.chauvin@estaca.eu>
2021-09-02 10:16:59 +0200

    connexion

--- musique-web/src/main/java/fr/musique/config/WebConfig.java
+++ musique-web/src/main/java/fr/musique/config/WebConfig.java
@@ -3,12 +3,20 @@ package fr.musique.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+	}
+	
 	@Bean
 	public UrlBasedViewResolver viewResolver() {
 		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
--- musique-web/src/main/java/fr/musique/controller/AccueilConnectionController.java
+++ musique-web/src/main/java/fr/musique/controller/AccueilConnectionController.java
@@ -13,27 +13,31 @@ import fr.musique.service.ConnectionService;
 
 @Controller
 public class AccueilConnectionController {
-	
+
 	@Autowired
 	private ConnectionService connexion;
-	
+
 	@Autowired
 	private ICompteDaoJpaRepository daoCompte;
-	
+
 	@GetMapping("/accueil-connection")
-	public String affichage(){
+	public String affichage() {
 		return "accueilConnection";
 	}
 
 	@PostMapping("/accueil-connection")
-	public String connection(@RequestParam String email,@RequestParam String password,Model model){
-		Compte compteAConnecter = daoCompte.findByEmail(email);
-		String c = "accueil-connection";
-		if (compteAConnecter.getPassword() != password){
-			c = "redirect:/accueil-connecte";
-		}else{
+	public String connection(@RequestParam String email, @RequestParam String password, Model model) {
+		
+		if (daoCompte.findByEmail(email) != null) {
+			if (daoCompte.findByEmail(email).getPassword().equals(password)) {
+				return "redirect:/accueil-connecte";
+			} else {
+				model.addAttribute("erreur", "Vous vous êtes trompé, béta !");
+				return "accueilConnection";
+			}
+		} else {
 			model.addAttribute("erreur", "Vous vous êtes trompé, béta !");
+			return "accueilConnection";
 		}
-		return c;
 	}
 }
